cc411b1ce698d22f8780faea58d3e1e502590997
zokan21 <zokan21@student.sdu.dk>
2021-03-18 13:29:38 +0100

    clear file

--- first_tries.py
+++ first_tries.py
@@ -14,39 +14,30 @@ cancer = data[data['Classification'] == 2]
 fig, axs = plt.subplots(3, 3)
 
 axs[0,0].boxplot([healthy['Age'], cancer['Age']])
-# axs[0,0].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[0,0].set_title('Age')
 
 axs[0,1].boxplot([healthy['BMI'], cancer['BMI']])
-# axs[0,1].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[0,1].set_title('BMI')
 
 axs[0,2].boxplot([healthy['Glucose'], cancer['Glucose']])
-# axs[0,2].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[0,2].set_title('Glucose')
 
 axs[1,0].boxplot([healthy['Insulin'], cancer['Insulin']])
-# axs[1,0].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[1,0].set_title('Insulin')
 
 axs[1,1].boxplot([healthy['HOMA'], cancer['HOMA']])
-# axs[1,1].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[1,1].set_title('HOMA')
 
 axs[1,2].boxplot([healthy['Leptin'], cancer['Leptin']])
-# axs[1,2].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[1,2].set_title('Leptin')
 
 axs[2,0].boxplot([healthy['Adiponectin'], cancer['Adiponectin']])
-# axs[2,0].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[2,0].set_title('Adiponectin')
 
 axs[2,1].boxplot([healthy['Resistin'], cancer['Resistin']])
-# axs[2,1].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[2,1].set_title('Resistin')
 
 axs[2,2].boxplot([healthy['MCP.1'], cancer['MCP.1']])
-# axs[2,2].set_xticklabels([1, 2], ['Healthy patients', 'Cancer patients'])
 axs[2,2].set_title('MCP.1')
 
 plt.show()
